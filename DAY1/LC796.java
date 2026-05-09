37cd0c7d7054d5ee999d678e13de17abfe2e11bc
aditya <rishabthakurji722@gmail.com>
2026-05-09 10:56:54 +0530

    Convert DAY1 to tracked source files and remove arrow comment

---
+++ DAY1/LC796.java
@@ -0,0 +1,12 @@
+
+class approach1 {
+
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        return (s + s).contains(goal);
+
+    }
+}
